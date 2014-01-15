61ee27e02a2e850fcd334fc270eb13c471ae4c1a
Chris Coffey <christopher.coffey@rackspace.com>
2014-01-15 16:46:20 -0600

    new testing files

---
+++ test/kitchen/cookbooks/rackspace-time_test/metadata.rb
@@ -0,0 +1,6 @@
+name             "rackspace-time_test"
+maintainer       "Christopher Coffey"
+maintainer_email "christopher.coffey@rackspace.com"
+license          "Apache 2.0"
+description      "Cookbook for kitchen test"
+version          "0.1.0"
---
+++ test/kitchen/cookbooks/rackspace-time_test/recipes/default.rb
@@ -0,0 +1,3 @@
+node.set[:rackspace_time][:use_symlink] = false
+node.set[:rackspace_time][:tz] = 'Africa/Timbuktu'
+include_recipe 'rackspace-time'
