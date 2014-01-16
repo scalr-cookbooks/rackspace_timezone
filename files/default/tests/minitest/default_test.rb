012b96220face6076a001dfedbc2f1ce5bf20cb0
Chris Coffey <christopher.coffey@rackspace.com>
2014-01-16 14:02:36 -0600

    changed testing variables

--- files/default/tests/minitest/default_test.rb
+++ files/default/tests/minitest/default_test.rb
@@ -2,10 +2,10 @@ require 'minitest/spec'
 
 class TimezoneIiSpec < MiniTest::Chef::Spec
 
-    describe_recipe 'timezone-ii::default' do
+    describe_recipe 'rackspace_time::default' do
 
         it 'updates the timezone' do
-            IO.read('/etc/localtime').must_equal IO.read("/usr/share/zoneinfo/#{node[:tz]}")
+            IO.read('/etc/localtime').must_equal IO.read("/usr/share/zoneinfo/#{node[:rackspace_time][:tz]}")
         end
 
     end
--- test/kitchen/cookbooks/rackspace-time_test/recipes/default.rb
+++ test/kitchen/cookbooks/rackspace-time_test/recipes/default.rb
@@ -1,3 +1,3 @@
 node.set[:rackspace_time][:use_symlink] = false
 node.set[:rackspace_time][:tz] = 'Africa/Timbuktu'
-include_recipe 'rackspace-time'
+include_recipe 'rackspace_time'
